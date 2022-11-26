e60775d4644eeaa71906562f6f2c182ea167a88a
ARJUN-TS <arjun.ts1@ibm.com>
2022-11-26 13:37:06 +0530

    added port in log

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ require("dotenv").config();
 const app=require("./index")
 const port=process.env.PORT || 3000
 app.listen(port, function(){
-    console.log(`listening on port ${process.env.NODE_PORT}`)
+    console.log(`listening on port ${port}`)
 })
