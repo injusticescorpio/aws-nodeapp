266c4d17ff50097eeb637786fc4b647b441193b9
ARJUN-TS <arjun.ts1@ibm.com>
2022-12-28 20:06:48 +0530

    added k8 and change port

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const app=require("./index")
-const port=process.env.PORT || 3000
+const port=process.env.PORT || 4000
 console.log(`post is ${port}`)
 app.listen(port, function(){
     console.log(`listening on port ${port}`)
